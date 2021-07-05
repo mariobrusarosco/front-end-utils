ce73b48de43c5e3b5b92a737fd6c433e3276a76f
Mario Brusarosco <mariobrusarosco@hotmail.com>
2021-07-05 09:41:28 -0300

    Basic 'Build' process

--- src/Logger/index.ts
+++ src/Logger/index.ts
@@ -1,1 +1,9 @@
-export const Logger = {};
+interface Logger {
+  log: Log;
+}
+
+type Log = (contentToLog: any) => void;
+
+export const Logger: Logger = {
+  log: (contentToLog) => console.log(contentToLog),
+};
